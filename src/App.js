3f4f996f4be9b85392be95da67c291df99c5f058
Angold-4 <wangold4w@gmail.com>
2023-06-02 12:11:10 +0800

    customized radar plot

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Radar from './Radar';
 import './App.css';
 
-const header1 = [50, 100, 150, 200, 250];
-const header2 = [250, 200, 150, 100, 50];
-const stockNames = ["Stock1", "Stock2", "Stock3", "Stock4", "Stock5"];
+const colors = [
+  "#e93f3a", "#eb4f4a", "#ed6562", "#f49b99", "#fce3e2",
+  "#d0ecdb", "#86cda2", "#49b373", "#1ca152", "#00953b",
+];
 
-const data = header1.map((value, i) => ({
-  x: value, 
-  y: header2[i], 
-  label: `Stock: ${stockNames[i]}\nX: ${value}\nY: ${header2[i]}`
+// Generating random data
+const numPoints = 3000;
+const stockNames = Array.from({ length: numPoints }, (_, i) => `Stock${i+1}`);
+const header1 = Array.from({ length: numPoints }, () => Math.random() * 250);
+const header2 = Array.from({ length: numPoints }, () => Math.random() * 250);
+const CRs = Array.from({ length: numPoints }, () => +(Math.random() * 10).toFixed(2));
+
+const data = stockNames.map((stock, i) => ({
+  x: header1[i],
+  y: header2[i],
+  cr: CRs[i],
+  color: colors[i % colors.length], // Reuse colors when i exceeds the length of colors array
+  label: `Stock: ${stock}\nCR: ${CRs[i]}\nX: ${header1[i]}\nY: ${header2[i]}`
 }));
-
 function App() {
   return (
     <div className="App">
@@ -20,7 +28,5 @@ function App() {
     </div>
   );
 }
-
-ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App;
--- src/Radar.js
+++ src/Radar.js
@@ -1,21 +1,73 @@
-import React from 'react';
-import { VictoryScatter, VictoryLabel, VictoryAxis } from 'victory';
+import { VictoryChart, VictoryScatter, VictoryTooltip, VictoryVoronoiContainer, VictoryAxis } from 'victory';
 
-const CustomScatterPlot = ({ data }) => {
+const Radar = ({ data }) => {
+  const inactiveSize = 1; // size of data points when not hovered
+  const activeSize = 5; // size of data points when hovered
+
   return (
     <svg viewBox="0 0 300 300" preserveAspectRatio="xMidYMid meet">
       <circle cx="150" cy="150" r="150" fill="black"/>
-      <VictoryScatter
+      <VictoryChart
         standalone={false}
         width={300} height={300}
-        data={data}
-        size={7}
-        style={{ data: { fill: "white" } }}
-        labels={({ datum }) => datum.label}
-        labelComponent={<VictoryLabel dy={-20}/>}
-      />
+        containerComponent={<VictoryVoronoiContainer />}
+      >
+        <VictoryScatter
+          data={data}
+          size={({ active }) => active ? activeSize : inactiveSize}
+          labels={({ datum, active }) => active ? datum.label : null}
+          style={{ 
+            data: { 
+              fill: ({ datum }) => datum.color,
+              stroke: ({ active }) => active ? "black" : "none",
+              strokeWidth: ({ active }) => active ? 2 : 0
+            } 
+          }}
+          labelComponent={
+            <VictoryTooltip 
+              renderInPortal={false}
+              orientation="top"
+              pointerLength={0}
+              cornerRadius={0}
+              flyoutStyle={{ fill: "white" }}
+            />
+          }
+
+          events={[
+            {
+              target: 'data',
+              eventHandlers: {
+                onMouseOver: () => {
+                  return [
+                    {
+                      target: 'data',
+                      mutation: (props) => ({ style: { ...props.style, fill: props.datum.color }, size: activeSize }) // increased size
+                    }, {
+                      target: 'labels',
+                      mutation: () => ({ active: true })
+                    }
+                  ];
+                },
+                onMouseOut: () => {
+                  return [
+                    {
+                      target: 'data',
+                      mutation: (props) => ({ style: { ...props.style, fill: props.datum.color }, size: inactiveSize }) // original size
+                    }, {
+                      target: 'labels',
+                      mutation: () => ({ active: false })
+                    }
+                  ];
+                }
+              }
+            }
+          ]}
+        />
+        <VictoryAxis style={{ axis: { visibility: 'hidden' }, ticks: { visibility: 'hidden' }, tickLabels: { visibility: 'hidden' } }} />
+        <VictoryAxis dependentAxis style={{ axis: { visibility: 'hidden' }, ticks: { visibility: 'hidden' }, tickLabels: { visibility: 'hidden' } }} />
+      </VictoryChart>
     </svg>
   );
 };
 
-export default CustomScatterPlot;
+export default Radar;
