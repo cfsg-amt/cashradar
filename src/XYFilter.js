4ca90b2d9596e024a2248355846ba7d1df854b88
Angold-4 <wangold4w@gmail.com>
2023-06-12 03:14:04 +0800

    add coordinate component

--- src/XYFilter.js
+++ src/XYFilter.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Checkbox } from '@mui/material';
+import { Checkbox, List, ListItem, Divider, Select, MenuItem, FormControl, OutlinedInput } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
-import { setSelectedGroups } from './redux/dataSlice';
+import { setSelectedGroups, setSelectedX, setSelectedY, fetchAdditionalDataForY, fetchAdditionalDataForX } from './redux/dataSlice';
 
 const XYFilter = () => {
   // Grab the colors array
@@ -12,37 +12,117 @@ const XYFilter = () => {
   
   const dispatch = useDispatch();
   
-  // Retrieve selectedGroups from Redux store
-  const selectedGroups = useSelector(state => state.data.selectedGroups);
+  // Retrieve necessary data from Redux store
+  const { selectedGroups, selectedX, selectedY, numHeaders } = useSelector(state => state.data);
 
   const handleCheckboxChange = (index) => {
     const newSelectedGroups = [...selectedGroups];
     
     if (newSelectedGroups.includes(index)) {
-      // Remove the index from the array if it's already included
       newSelectedGroups.splice(newSelectedGroups.indexOf(index), 1);
     } else {
-      // Add the index to the array if it's not already included
       newSelectedGroups.push(index);
     }
     
-    // Dispatch an action to update the selectedGroups in the Redux store
     dispatch(setSelectedGroups(newSelectedGroups));
+  };
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+      },
+    },
+  };
+
+  const selectPlaceholder = (value, handleChange) => (
+    <FormControl sx={{ m: 1, width: '100%', mt: 1 }}>
+      <Select
+        displayEmpty
+        value={value}
+        onChange={handleChange}
+        input={
+          <OutlinedInput 
+            sx={{
+              color: 'black',
+              '& .MuiOutlinedInput-root': {
+                borderColor: 'black', 
+                height: '30px', 
+              },
+              '& .MuiOutlinedInput-notchedOutline': {
+                borderColor: 'black', 
+              },
+              '& .MuiOutlinedInput-input': {
+                color: 'black'
+              },
+              '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                borderColor: 'black' 
+              },
+            }}
+          />
+        }
+        renderValue={(selected) => {
+          if (selected === "") {
+            return <em>Placeholder</em>;
+          }
+          return selected;
+        }}
+        MenuProps={MenuProps}
+        sx={{ 
+          color: 'black', // Changes the color of the dropdown indicator
+          fontFamily: 'SimSun', // Changes the font family
+          fontWeight: 'bold', // Makes the text bold
+          '& .MuiSelect-icon': {
+            color: 'black', // Changes the color of the dropdown indicator
+          },
+        }}
+        inputProps={{ 'aria-label': 'Without label' }}
+      >
+        <MenuItem disabled value="">
+          <em>Placeholder</em>
+        </MenuItem>
+        {numHeaders.map((header, index) => (
+          <MenuItem key={index} value={header} sx={{ fontFamily: 'SimSun', fontWeight: 'bold' }}>{header}</MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+
+  const handleSelectChange = (event, type) => {
+    if (type === 'X') {
+      dispatch(setSelectedX(event.target.value));
+      dispatch(fetchAdditionalDataForX(event.target.value));
+    } else if (type === 'Y') {
+      dispatch(setSelectedY(event.target.value));
+      dispatch(fetchAdditionalDataForY(event.target.value));
+    }
   };
 
   return (
-    <div className="XYFilter">
-      {[...Array(10).keys()].map((num, index) => (
-        <Checkbox
-          key={index}
-          color="default"
-          checked={selectedGroups.includes(index)}
-          onChange={() => handleCheckboxChange(index)}
-          inputProps={{ 'aria-label': `checkbox ${num}` }}
-          sx={{ '& .MuiSvgIcon-root': { fill: colors[num] } }}
-        />
-      ))}
-    </div>
+    <List className="XYFilter" sx={{ width: '100%' }}>
+      <ListItem sx={{ padding: 0, display: 'flex', alignItems: 'center' }}>
+        {[...Array(10).keys()].map((num, index) => (
+          <Checkbox
+            key={index}
+            color="default"
+            checked={selectedGroups.includes(index)}
+            onChange={() => handleCheckboxChange(index)}
+            inputProps={{ 'aria-label': `checkbox ${num}` }}
+            sx={{ '& .MuiSvgIcon-root': { fill: colors[num] } }}
+          />
+        ))}
+      </ListItem>
+      <Divider light sx={{ bgcolor: '#bbb' }}/>
+      <ListItem sx={{ padding: 0, display: 'flex', alignItems: 'center' }}>
+        {selectPlaceholder(selectedX, (e) => handleSelectChange(e, 'X'))}
+      </ListItem>
+      <Divider light sx={{ bgcolor: '#bbb' }}/>
+      <ListItem sx={{ padding: 0, display: 'flex', alignItems: 'center' }}>
+        {selectPlaceholder(selectedY, (e) => handleSelectChange(e, 'Y'))}
+      </ListItem>
+    </List>
   );
 };
 
