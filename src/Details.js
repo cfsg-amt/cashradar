eb37906fbf71377d8b3d74f64e45316bd06b2859
Angold-4 <wangold4w@gmail.com>
2023-06-15 06:54:10 +0800

    finished debugging, now able to display the right bar with color

--- src/Details.js
+++ src/Details.js
@@ -15,6 +15,22 @@ const ColorLinearProgress = styled(LinearProgress)(({ theme, index }) => ({
   },
 }));
 
+const CustomColorLinearProgress = styled(LinearProgress)(({ theme, value }) => {
+  const colors = [
+    "#e93f3a", "#eb4f4a", "#ed6562", "#f49b99", "#fce3e2",
+    "#d0ecdb", "#86cda2", "#49b373", "#1ca152", "#00953b",
+  ];
+  const index = Math.floor(value / 10); // Adjusted index calculation
+  return ({
+    height: 20,
+    borderRadius: 5,
+    backgroundColor: theme.palette.grey[300],
+    '& .MuiLinearProgress-bar': {
+      backgroundColor: colors[index] || theme.palette.primary.main, // fallback to primary color if index is out of range
+    },
+  });
+});
+
 // Apply font and color
 const CustomTypography = styled(Typography)(({ theme }) => ({
   color: theme.palette.grey[800],
@@ -23,7 +39,6 @@ const CustomTypography = styled(Typography)(({ theme }) => ({
 }));
 
 const Details = () => {
-
   const [stockData, setStockData] = useState(null);
   const searchName = useSelector(state => state.data.searchName);
   const region = useSelector(state => state.data.region);
@@ -53,21 +68,55 @@ const Details = () => {
       fields: ['技術分析標準分數', '價格及交易量變化比率分数', '調整後移動平均線标准分数', '調整後動向指標分数', '相對強弱指數标准分数', '布林线指數标准分数']
     },
     {
-      title: 'Others',
+      title: '其他指數',
       fields: ['相對強弱指數 (9日)', '布林线 (下線) (20日)', '波幅指數 (10日)', '移動平均線 (5日)', '每首股數', '預測1年股息回報', '分析員分數', '分析員分數變化', '大行中位目標價']
     },
   ];
 
   return (
-    <Box sx={{ maxWidth: 1200, margin: '0 auto', px: 3 }}> {/* px for some padding */}
+    <Box sx={{ maxWidth: 1200, margin: '0 auto', px: 3 }}>
+      {stockData && (
+        <Accordion>
+          <AccordionSummary>
+            <Grid container alignItems="center">
+              <Grid item xs={12}>
+                <CustomTypography variant="h6">
+                  {"時富雷達 (CR)"}: {stockData["時富雷達 (CR)"]}
+                </CustomTypography>
+              </Grid>
+              <Grid item xs={12}>
+                <CustomColorLinearProgress
+                  variant="determinate"
+                  value={(stockData["時富雷達 (CR)"] / 10) * 100}
+                />
+              </Grid>
+            </Grid>
+          </AccordionSummary>
+        </Accordion>
+      )}
       {stockData && accordionGroups.map((group, index) => (
         <Accordion key={index}>
           <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-            <CustomTypography variant="h6">{`${group.title} ${group.fields[0]}: ${stockData[group.fields[0]]}`}</CustomTypography>
+            <Grid container alignItems="center">
+              <Grid item xs={12}>
+                <CustomTypography variant="h6">
+                  {index < 2 ? `${group.title} ${group.fields[0]}: ${stockData[group.fields[0]]}` : group.title}
+                </CustomTypography>
+              </Grid>
+              {index < 2 && 
+                <Grid item xs={12}>
+                  <ColorLinearProgress
+                    variant="determinate"
+                    value={(stockData[group.fields[0]] - minMaxData.min[group.fields[0]]) / (minMaxData.max[group.fields[0]] - minMaxData.min[group.fields[0]]) * 100}
+                    index={index}
+                  />
+                </Grid>
+              }
+            </Grid>
           </AccordionSummary>
           <AccordionDetails>
             <Grid container spacing={2}>
-              {group.fields.map(field => (
+              {group.fields.slice(index < 2 ? 1 : 0).map(field => (
                 <Grid item xs={12} sm={6} md={4} key={field}>
                   <Card variant="outlined">
                     <CardContent>
@@ -81,7 +130,7 @@ const Details = () => {
                           </Tooltip>
                         </Grid>
                       </Grid>
-                      <Typography variant="body1" color="text.secondary">{stockData[field]}</Typography> {/* use MUI's secondary text color */}
+                      <Typography variant="body1" color="text.secondary">{stockData[field]}</Typography>
                       <ColorLinearProgress 
                         variant="determinate" 
                         value={(stockData[field] - minMaxData.min[field]) / (minMaxData.max[field] - minMaxData.min[field]) * 100} 
