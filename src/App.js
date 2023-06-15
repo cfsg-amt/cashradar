f5d5461a370247f3f92670fe0f057c11c340e00c
Angold-4 <wangold4w@gmail.com>
2023-06-15 19:34:12 +0800

    update router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Radar from './Radar';
+import { BrowserRouter as Router } from "react-router-dom";
 import RadarTab from './RadarTab';
 import XYFilter from './XYFilter';
 import Search from './Search';
@@ -40,45 +41,47 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <div>
-        <div className="App" style={{backgroundColor: isMobile ? 'white' : tabletBackgroundColor}}>
-          <AppBar position="static" style={{ backgroundColor: '#ea5504' }}>
-            <Toolbar>
-              <IconButton edge="start" color="inherit" aria-label="menu">
-                <img src={process.env.PUBLIC_URL + '/static/cfsg.svg'} alt="logo" style={{width: "200px"}}/>
-              </IconButton>
-              <Box flexGrow={1}></Box> {/* Add this Box component to push the subsequent items to the right */}
-              <IconButton edge="end" color="inherit" aria-controls="simple-menu" aria-haspopup="true" onClick={handleMenuClick}>
-                <MenuIcon /> {/* Replace button text with Menu icon */}
-              </IconButton>
-              <Menu
-                id="simple-menu"
-                anchorEl={anchorEl}
-                keepMounted
-                open={Boolean(anchorEl)}
-                onClose={handleMenuClose}
-              >
-                <MenuItem onClick={handleMenuClose} component={Link} to="/news">主頁</MenuItem>
-                <MenuItem onClick={handleMenuClose} component={Link} to="/news">宏觀經濟</MenuItem>
-                <MenuItem onClick={handleMenuClose} component={Link} to="/">時富雷達</MenuItem>
-              </Menu>
-            </Toolbar>
-          </AppBar>
-          <RadarTab onIconClick={handleIconClick} />  {/* pass the function as a prop */}
-          {isVisible && (  // only render the components if isVisible is true
-            <Grid container spacing={0} style={{ maxWidth: '1200px', marginLeft: 'auto', marginRight: 'auto' }}>
-              <Grid item xs={12} sm={12} md={6}>
-                <Radar />
+      <Router>
+        <div>
+          <div className="App" style={{backgroundColor: isMobile ? 'white' : tabletBackgroundColor}}>
+            <AppBar position="static" style={{ backgroundColor: '#ea5504' }}>
+              <Toolbar>
+                <IconButton edge="start" color="inherit" aria-label="menu">
+                  <img src={process.env.PUBLIC_URL + '/static/cfsg.svg'} alt="logo" style={{width: "200px"}}/>
+                </IconButton>
+                <Box flexGrow={1}></Box> {/* Add this Box component to push the subsequent items to the right */}
+                <IconButton edge="end" color="inherit" aria-controls="simple-menu" aria-haspopup="true" onClick={handleMenuClick}>
+                  <MenuIcon /> {/* Replace button text with Menu icon */}
+                </IconButton>
+                <Menu
+                  id="simple-menu"
+                  anchorEl={anchorEl}
+                  keepMounted
+                  open={Boolean(anchorEl)}
+                  onClose={handleMenuClose}
+                >
+                  <MenuItem onClick={handleMenuClose} component={Link} to="/news">主頁</MenuItem>
+                  <MenuItem onClick={handleMenuClose} component={Link} to="/news">宏觀經濟</MenuItem>
+                  <MenuItem onClick={handleMenuClose} component={Link} to="/">時富雷達</MenuItem>
+                </Menu>
+              </Toolbar>
+            </AppBar>
+            <RadarTab onIconClick={handleIconClick} />  {/* pass the function as a prop */}
+            {isVisible && (  // only render the components if isVisible is true
+              <Grid container spacing={0} style={{ maxWidth: '1200px', marginLeft: 'auto', marginRight: 'auto' }}>
+                <Grid item xs={12} sm={12} md={6}>
+                  <Radar />
+                </Grid>
+                <Grid item xs={12} sm={12} md={6}>
+                  <XYFilter />
+                </Grid>
               </Grid>
-              <Grid item xs={12} sm={12} md={6}>
-                <XYFilter />
-              </Grid>
-            </Grid>
-          )}
-          <Search />
-          <Details />
+            )}
+            <Search />
+            <Details />
+          </div>
         </div>
-      </div>
+      </Router>
     </ThemeProvider>
   );
 }
